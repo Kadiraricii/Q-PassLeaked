eb0f6edb7df5977547e1bebeec4090420f484588
Keyvan Arasteh <16303698+keyvanarasteh@users.noreply.github.com>
2025-03-25 02:59:56 +0300

    bugs cleared, only pywin32 remaining

--- ux/nmap.py
+++ ux/nmap.py
@@ -1,5 +1,5 @@
 # ux/nmap.py
-from kivy.uix.screen import Screen
+from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
